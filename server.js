fe74cae08a9958e7311294a2547a4b155e4d8904
ivica <inikolovbiz@gmail.com>
2017-11-21 11:40:10 +0100

    commit during restructuring

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-
-
 const Hapi = require('hapi');
-
 const Inert = require('inert');
-
 const Vision = require('vision');
-
-const uuid = require('uuid');
-
 const Hanlebars = require('handlebars');
-
-const fs = require('fs');
-
-const Joi = require('joi');
-
-const Boom = require('boom');
-
 const Good = require('good');
-
-function loadCards() {
-	const file = fs.readFileSync('./cards.json');
-	return JSON.parse(file.toString());
-}
-
-const cards = loadCards();
+const Routes = require('./lib/routes');
 
 const goodOptions = {
 	ops: {
@@ -81,7 +61,7 @@ const provision = async () => {
 		options: goodOptions,
 	}, (err) => {
 		if (err) {
-			return console.error(err);
+			console.error(err);
 		}
 	});
 	await server.register(Vision);
@@ -91,6 +71,8 @@ const provision = async () => {
 		relativeTo: __dirname,
 		path: './templates',
 	});
+
+	server.route(Routes);
 
 	await server.start();
 	console.log('Server running at:', server.info.uri);
@@ -114,24 +96,6 @@ provision();
 // 	path: './templates',
 // });
 
-// Add route 2
-server.route({
-	method: 'GET',
-	path: '/hello/{name}',
-	handler: function (request, reply) {
-		const response = reply(`Hello ${encodeURIComponent(request.params.name)} from ${encodeURIComponent(request.info.remoteAddress)}:${encodeURIComponent(request.info.remotePort)} !`);
-		response.statusCode = 403;
-	},
-});
-
-server.route({
-	method: 'GET',
-	path: '/test',
-	handler: function (request, reply) {
-		reply.redirect('/hello/redir');
-	},
-});
-
 // server.ext('onRequest', (request, reply) => {
 // 	console.log(`Request received: ${request.path}`);
 // 	reply.continue();
@@ -153,74 +117,6 @@ server.register(Inert, () => {
 			file: 'templates/index.html',
 		},
 	});
-});
-
-function cardHandler(request, reply) {
-	reply.view('cards', { cards: cards });
-}
-
-function saveCard(card) {
-	const id = uuid.v1();
-	card.id = id;
-	cards[id] = card;
-}
-
-function mapImages() {
-	return fs.readdirSync('./public/images/cards');
-}
-
-const cardSchema = Joi.object().keys({
-	name: Joi.string().min(3).max(50).required(),
-	recipient_email: Joi.string().email(),
-	sender_name: Joi.string().min(3).max(50).required(),
-	sender_email: Joi.string().email(),
-	card_image: Joi.string().regex(/.+\.(jpg|bpm|png|gif)\b/).required(),
-});
-
-function newCardHandler(request, reply) {
-	// business logic
-	if (request.method === 'get') {
-		reply.view('new', { card_images: mapImages() });
-	} else {
-		Joi.validate(request.payload, cardSchema, (err, val) => {
-			if (err) {
-				return reply(Boom.badRequest(err.details[0].message));
-			}
-			const card = {
-				name: val.name,
-				recipient_email: val.recipient_email,
-				sender_name: val.sender_name,
-				sender_email: val.sender_email,
-				card_image: val.card_image,
-			};
-			saveCard(card);
-			console.log(cards);
-			return reply.redirect('/cards');
-		});
-	}
-}
-
-function deleteCardHandler(request, reply) {
-	delete cards[request.params.id];
-	reply();
-}
-
-server.route({
-	method: 'GET',
-	path: '/cards',
-	handler: cardHandler,
-});
-
-server.route({
-	method: ['GET', 'POST'],
-	path: '/cards/new',
-	handler: newCardHandler,
-});
-
-server.route({
-	method: 'DELETE',
-	path: '/cards/{id}',
-	handler: deleteCardHandler,
 });
 
 // Add route 3 - directory exposure
